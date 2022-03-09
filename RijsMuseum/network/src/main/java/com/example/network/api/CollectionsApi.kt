8ae73c1b42a35ca5c77e21aaa4147b8046099d88
XCMachine <69400350+XCMachine@users.noreply.github.com>
2022-03-09 14:30:12 +0200

    Update CollectionsApi.kt, added parameters within the method getCollections().

--- RijsMuseum/network/src/main/java/com/example/network/api/CollectionsApi.kt
+++ RijsMuseum/network/src/main/java/com/example/network/api/CollectionsApi.kt
@@ -3,15 +3,16 @@ package com.example.network.api
 import com.example.network.models.Collections
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface CollectionsApi {
-    //Base URL: https://www.rijksmuseum.nl
-
     //Gets the collections api
-    @GET("/api/nl/collection/SK-C-5?key=Z8ZQgAb8&format=json&culture=en&p=0&ps=10")
-    fun getCollections(): Call<List<Collections>>
-
-    //Gets the collection details api
-    @GET("/api/nl/collection/SK-C-5?key=Z8ZQgAb8&format=json&culture=en")
-    fun getCollectionsDetails(): Call<List<CollectionsDetails>>
+    @GET("/api/{culture}/collection/SK-C-5?key=Z8ZQgAb8&format=json&culture=en&p=0&ps=10")
+    fun getCollections(
+        @Path("culture") culture: String,
+        @Query("format") format: String = "json",
+        @Query("p") p: String = "0",
+        @Query("ps") ps: String = "10"
+    ): Call<List<Collections>>
 }
