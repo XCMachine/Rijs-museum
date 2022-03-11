218d06aec6bc68f60f504f55f1e473efea105f0a
XCMachine <69400350+XCMachine@users.noreply.github.com>
2022-03-11 16:00:57 +0200

    Update CollectionsApi.kt, added a new method getCollectionsDetails() and updated the get request url.

--- RijsMuseum/network/src/main/java/com/example/network/api/CollectionsApi.kt
+++ RijsMuseum/network/src/main/java/com/example/network/api/CollectionsApi.kt
@@ -1,6 +1,7 @@
 package com.example.network.api
 
 import com.example.network.models.Collections
+import com.example.network.models.CollectionsDetails
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -8,11 +9,18 @@ import retrofit2.http.Query
 
 interface CollectionsApi {
     //Gets the collections api
-    @GET("/api/{culture}/collection/SK-C-5?key=Z8ZQgAb8")
+    @GET("/api/nl/collection?key=Z8ZQgAb8&involvedMaker=Rembrandt+van+Rijn")
     fun getCollections(
         @Path("culture") culture: String,
         @Query("format") format: String = "json",
         @Query("p") page: String = "0",
         @Query("ps") pages: String = "10"
     ): Call<List<Collections>>
+
+    //GEts the collections details api
+    @GET("/api/{culture}/collection/SK-C-5?key=Z8ZQgAb8")
+    fun getCollectionsDetails(
+        @Path("culture") culture: String,
+        @Query("format") format: String
+    ): Call<List<CollectionsDetails>>
 }
