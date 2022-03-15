a09953124d70f715cc64b0b05d85a155228396d6
XCMachine <69400350+XCMachine@users.noreply.github.com>
2022-03-15 17:33:09 +0200

    Update Retrofit.kt

--- RijsMuseum/network/src/main/java/com/example/network/Retrofit.kt
+++ RijsMuseum/network/src/main/java/com/example/network/Retrofit.kt
@@ -1,13 +1,19 @@
 package com.example.network
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class Retrofit {
+    private val interceptor = HttpLoggingInterceptor()
+    private val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
+
     val builder: Retrofit = Retrofit.Builder()
-            .baseUrl(baseUrl)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+        .baseUrl(baseUrl)
+        .client(client)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
 
     companion object {
         const val baseUrl = "https://www.rijksmuseum.nl"
