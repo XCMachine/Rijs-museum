c8a8c31f74621d9c9c47d801b91fdafbc934e3ea
XCMachine <69400350+XCMachine@users.noreply.github.com>
2022-03-15 10:51:21 +0200

    Update MainActivity.kt

--- RijsMuseum/app/src/main/java/com/example/rijsmuseum/MainActivity.kt
+++ RijsMuseum/app/src/main/java/com/example/rijsmuseum/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.example.rijsmuseum
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
+import com.example.rijsmuseum.adapter.RecyclerAdapter
+import com.example.rijsmuseum.viewmodel.MainViewModel
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), RecyclerAdapter.OnItemClickListener {
+    private lateinit var getRequestButton: Button
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: RecyclerAdapter
+
+    private var viewModel: MainViewModel? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        getRequestButton = findViewById(R.id.getRequestButton)
+
+        recyclerView = findViewById(R.id.listItems)
+        recyclerView.hasFixedSize()
+
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        viewModel?.let { mainModel ->
+            getRequestButton.setOnClickListener {
+                mainModel.getCollectionsRequest()
+            }
+        }
+
+        adapter = RecyclerAdapter(viewModel!!.exampleList!!, this)
+        recyclerView.adapter = adapter
+    }
+
+    //This is for selected artifact.
+    override fun onItemClick(position: Int) {
+        viewModel?.let {
+            //not used yet
+            val clickedItem = it.exampleList!![position]
+        }
     }
 }
