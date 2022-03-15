ed8190661c2867bfc8ceadbea9ad0a79bc8a6bf5
XCMachine <69400350+XCMachine@users.noreply.github.com>
2022-03-15 15:48:33 +0200

    Update MainActivity.kt

--- RijsMuseum/app/src/main/java/com/example/rijsmuseum/MainActivity.kt
+++ RijsMuseum/app/src/main/java/com/example/rijsmuseum/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.rijsmuseum.adapter.RecyclerAdapter
 import com.example.rijsmuseum.viewmodel.MainViewModel
@@ -11,35 +12,31 @@ import com.example.rijsmuseum.viewmodel.MainViewModel
 class MainActivity : AppCompatActivity(), RecyclerAdapter.OnItemClickListener {
     private lateinit var getRequestButton: Button
     private lateinit var recyclerView: RecyclerView
-    private lateinit var adapter: RecyclerAdapter
 
-    private var viewModel: MainViewModel? = null
+    private lateinit var viewModel: MainViewModel
+    private lateinit var adapter: RecyclerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         getRequestButton = findViewById(R.id.getRequestButton)
 
-        recyclerView = findViewById(R.id.listItems)
-        recyclerView.hasFixedSize()
-
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
-        viewModel?.let { mainModel ->
-            getRequestButton.setOnClickListener {
-                mainModel.getCollectionsRequest()
-            }
-        }
+        recyclerView = findViewById(R.id.listItems)
+        recyclerView.hasFixedSize()
+        recyclerView.layoutManager = LinearLayoutManager(this)
 
         //THIS NEEDS TO BE IN THE GETTER CLASS
-        adapter = RecyclerAdapter(viewModel!!.exampleList!!, this)
+        adapter = RecyclerAdapter(viewModel.exampleList, this)
         recyclerView.adapter = adapter
         adapter.setOnItemClickListener(this)
+        viewModel.getCollectionsRequest()
     }
 
     //This is for selected artifact.
     override fun onItemClick(position: Int) {
-        viewModel?.let {
+        viewModel.let {
             //not used yet
             //val clickedItem = it.exampleList!![position]
         }
