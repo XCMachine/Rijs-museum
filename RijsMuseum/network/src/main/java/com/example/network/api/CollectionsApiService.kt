b009a7e9404e8eccfb7068dfc1877c0d20e85eff
XCMachine <69400350+XCMachine@users.noreply.github.com>
2022-03-16 16:35:05 +0200

    Update CollectionsApiService.kt in network module,

--- RijsMuseum/network/src/main/java/com/example/network/api/CollectionsApiService.kt
+++ RijsMuseum/network/src/main/java/com/example/network/api/CollectionsApiService.kt
@@ -7,10 +7,10 @@ import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
 
-interface CollectionsApi {
+interface CollectionsApiService {
     //Gets the collections api
     @GET("/api/{culture}/collection")
-    fun getCollections(
+    fun getCollectionsList(
         @Path("culture") culture: String,
         @Query("format") format: String = "json",
         @Query("p") page: String = "0",
@@ -20,12 +20,14 @@ interface CollectionsApi {
 
     //GEts the collections details api
     @GET("/api/{culture}/collection/{objectNumber}")
-    fun getCollectionsDetails(
+    fun getCollectionsDetailsList(
         @Path("culture") culture: String,
         @Path("objectNumber") objectNumber: String,
         @Query("format") format: String,
         @Query("key") key: String = KEY
     ): Call<CollectionsDetails>
-}
 
-const val KEY = "Z8ZQgAb8"
+    companion object {
+        const val KEY = "Z8ZQgAb8"
+    }
+}
