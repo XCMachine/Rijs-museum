f312f022dc88cdca0ac6ca80c666e23a9d24f4a7
XCMachine <69400350+XCMachine@users.noreply.github.com>
2022-03-16 16:38:54 +0200

    Update RecyclerAdapter.kt

--- RijsMuseum/app/src/main/java/com/example/rijsmuseum/adapter/RecyclerAdapter.kt
+++ RijsMuseum/app/src/main/java/com/example/rijsmuseum/adapter/RecyclerAdapter.kt
@@ -1,6 +1,5 @@
 package com.example.rijsmuseum.adapter
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,17 +9,11 @@ import com.example.network.models.Collections
 import com.example.rijsmuseum.MainActivity
 import com.example.rijsmuseum.R
 
-class RecyclerAdapter(listItems: List<Collections>, var context: Context) : RecyclerView.Adapter<RecyclerAdapter.RecyclerViewHolder>() {
-    //We are assigning the exampleList ArrayList<Articles> and connecting it with the class 'listItem' parameter
-    private var exampleList: List<Collections> = listItems
+class RecyclerAdapter(listItems: List<Collections>) : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+    private var exampleList: ArrayList<Collections> = listItems as ArrayList<Collections>
 
-    //Using the listener for the interface later on
     private var mListener: OnItemClickListener? = null
 
-    /**
-     * This interface stands for the Summary to get every single summary on each individual item and passes JSON data within them.
-     * It includes the onItemClick function and it's parameter to pass it on 'position' type Int
-     */
     interface OnItemClickListener {
         fun onItemClick(position: Int)
     }
@@ -28,37 +21,30 @@ class RecyclerAdapter(listItems: List<Collections>, var context: Context) : Recy
         mListener = listener
     }
 
-    /** Connecting the context then making a LayoutInflater along with it's context of the activity, then selecting the card_items.xml, then using the ViewGroup, making sure
-     * to put attachToRoot into false
-     */
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewHolder {
-        val v = LayoutInflater.from(context).inflate(R.layout.card_items, parent, false)
-        return RecyclerViewHolder(v)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.card_items, parent, false)
+        return ViewHolder(view)
     }
 
-    /**
-     * We connect the ViewHolder class that is in the ViewGroup and then using the those texts to get the JSON data and using listItems original position of that list
-     */
-    override fun onBindViewHolder(holder: RecyclerViewHolder, position: Int) {
-        val listItem = exampleList[position]
-
-        holder.titleText.text = listItem.artObjects[position].title
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(exampleList[position], position)
     }
 
-    /**
-     * Getting the item count of the max size of the list of items
-     */
-    override fun getItemCount(): Int = exampleList.size
+    override fun getItemCount(): Int {
+        return exampleList.size
+    }
 
-    /**
-     * We use 'inner' keyword in order to have access and use the new interface a.k.a. 'mListener' and initializing the TextViews to it's ID here!
-     */
-    inner class RecyclerViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-        var titleText: TextView = itemView.findViewById(R.id.titleText)
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        lateinit var titleText: TextView
+
+        fun bind(data: Collections, position: Int) {
+            titleText.text = data.artObjects[position].title
+        }
 
         init {
             //Setting the new interface listener here, using it's position parameter
             itemView.setOnClickListener {
+                titleText = itemView.findViewById(R.id.titleText)
                 if (mListener != null) {
                     val position = adapterPosition
                     if (position != RecyclerView.NO_POSITION) {
@@ -69,8 +55,7 @@ class RecyclerAdapter(listItems: List<Collections>, var context: Context) : Recy
         }
     }
 
-    //Making sure that the exampleList variable is connected to the listItems parameter within the RecyclerAdapter class
     init {
-        exampleList = listItems
+        exampleList = listItems as ArrayList<Collections>
     }
 }
