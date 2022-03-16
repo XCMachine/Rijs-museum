b26a7fa30fb202feaa3cf43ce5cbbb9226a2912c
XCMachine <69400350+XCMachine@users.noreply.github.com>
2022-03-16 16:40:57 +0200

    Update MainActivity.kt

--- RijsMuseum/app/src/main/java/com/example/rijsmuseum/MainActivity.kt
+++ RijsMuseum/app/src/main/java/com/example/rijsmuseum/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.rijsmuseum
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -9,11 +10,12 @@ import com.example.rijsmuseum.adapter.RecyclerAdapter
 import com.example.rijsmuseum.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity(), RecyclerAdapter.OnItemClickListener {
-    //private lateinit var getRequestButton: Button
+    private lateinit var getRequestButton: Button
     private lateinit var recyclerView: RecyclerView
 
     private lateinit var viewModel: MainViewModel
-    private var adapter: RecyclerAdapter? = null
+
+    private lateinit var adapter: RecyclerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,25 +24,31 @@ class MainActivity : AppCompatActivity(), RecyclerAdapter.OnItemClickListener {
 
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
-        recyclerView = findViewById(R.id.listItems)
+        recyclerView = findViewById(R.id.recyclerView)
         recyclerView.hasFixedSize()
+
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        viewModel.getCollectionsObserver()
+
+        adapterSettings()
+
+        getRequestButton = findViewById(R.id.getButton)
+        getRequestButton.setOnClickListener {
+            viewModel.getCollectionsRequest()
+        }
+    }
+
+    private fun adapterSettings() {
+        adapter = RecyclerAdapter(viewModel.exampleList)
         recyclerView.layoutManager = LinearLayoutManager(this)
-
-        viewModel.getCollectionsRequest()
-
-        //Adapter settings from the ViewModel
-        adapter = RecyclerAdapter(viewModel.exampleList, this)
-        viewModel.adapterSettings(
-            adapter = adapter!!,
-            recyclerView = recyclerView,
-            listener = this
-        )
+        recyclerView.adapter = adapter
+        adapter.setOnItemClickListener(this)
     }
 
     //This is for selected artifact.
     override fun onItemClick(position: Int) {
         viewModel.let {
-            //not used yet
+            //It's commented because it won't be used yet, need to display the list first.
             //val clickedItem = it.exampleList!![position]
         }
     }
