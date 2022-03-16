e510c06059e362fd68c97a02c9a76446e0a7147c
XCMachine <69400350+XCMachine@users.noreply.github.com>
2022-03-16 16:41:18 +0200

    Update MainViewModel.kt

--- RijsMuseum/app/src/main/java/com/example/rijsmuseum/viewmodel/MainViewModel.kt
+++ RijsMuseum/app/src/main/java/com/example/rijsmuseum/viewmodel/MainViewModel.kt
@@ -1,27 +1,23 @@
 package com.example.rijsmuseum.viewmodel
 
 import androidx.lifecycle.ViewModel
-import androidx.recyclerview.widget.RecyclerView
 import com.example.network.getters.CollectionsDetailsGetter
 import com.example.network.getters.CollectionsGetter
 import com.example.network.models.Collections
-import com.example.rijsmuseum.MainActivity
-import com.example.rijsmuseum.adapter.RecyclerAdapter
 
 class MainViewModel: ViewModel() {
-    val exampleList: List<Collections> = listOf()
+    var exampleList: MutableList<Collections> = mutableListOf()
+
+    init {
+        exampleList = mutableListOf()
+    }
+
+    fun getCollectionsObserver(): MutableList<Collections> {
+        return exampleList
+    }
 
     fun getCollectionsRequest() {
         CollectionsGetter().getCollectionRequest()
-    }
-
-    fun adapterSettings(
-        adapter: RecyclerAdapter,
-        recyclerView: RecyclerView,
-        listener: MainActivity
-    ) {
-        recyclerView.adapter = adapter
-        adapter.setOnItemClickListener(listener)
     }
 
     fun getCollectionsDetailsRequest() {
