7353d23224e359e443bc935efacb8500cbdfd9ab
XCMachine <69400350+XCMachine@users.noreply.github.com>
2022-03-17 15:44:09 +0200

    Update MainViewModel.kt

--- RijsMuseum/app/src/main/java/com/example/rijsmuseum/viewmodel/MainViewModel.kt
+++ RijsMuseum/app/src/main/java/com/example/rijsmuseum/viewmodel/MainViewModel.kt
@@ -4,16 +4,20 @@ import androidx.lifecycle.ViewModel
 import com.example.network.getters.CollectionsDetailsGetter
 import com.example.network.getters.CollectionsGetter
 import com.example.network.models.Collections
+import com.example.network.models.CollectionsDetails
 
 class MainViewModel: ViewModel() {
+    //Collections model
     var artObjects: MutableList<Collections.ArtObject> = mutableListOf()
+    //CollectionsDetails model
+    var artObjectsDetails: MutableList<CollectionsDetails.ArtObject> = mutableListOf()
 
     fun getCollectionsRequest(callback: CollectionsGetter.DataReadyCallback) {
         CollectionsGetter().getCollectionRequest(callback)
     }
 
-    fun getCollectionsDetailsRequest() {
-        CollectionsDetailsGetter().getCollectionsDetailsRequest()
+    fun getCollectionsDetailsRequest(callbackDetails: CollectionsDetailsGetter.DataReadyCallback) {
+        CollectionsDetailsGetter().getCollectionsDetailsRequest(callbackDetails)
     }
 
 
