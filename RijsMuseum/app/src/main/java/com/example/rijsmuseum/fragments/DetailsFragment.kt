fc527e6803f4ba6987f90bf5b56d99ef5b395b8d
XCMachine <69400350+XCMachine@users.noreply.github.com>
2022-03-18 13:51:05 +0200

    Renamed and Updated CollectionsDetailsFragment.kt

--- RijsMuseum/app/src/main/java/com/example/rijsmuseum/fragments/DetailsFragment.kt
+++ RijsMuseum/app/src/main/java/com/example/rijsmuseum/fragments/DetailsFragment.kt
@@ -6,11 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.rijsmuseum.R
+import com.example.rijsmuseum.viewmodel.DetailsViewModel
 
 
-class CollectionsDetailsFragment : Fragment(R.layout.fragment_collection_details) {
+class DetailsFragment : Fragment(R.layout.fragment_collection_details) {
     private lateinit var titleText: TextView
+    private lateinit var detailsViewModel: DetailsViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,5 +29,6 @@ class CollectionsDetailsFragment : Fragment(R.layout.fragment_collection_details
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         titleText = view.findViewById(R.id.fragmentTitle)
+        detailsViewModel = ViewModelProvider(this).get(DetailsViewModel::class.java)
     }
 }
