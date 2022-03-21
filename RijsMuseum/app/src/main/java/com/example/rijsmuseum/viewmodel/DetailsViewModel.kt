30364008e75fe2313fbc6695a4a1dfcf4839fc17
XCMachine <69400350+XCMachine@users.noreply.github.com>
2022-03-21 17:21:42 +0200

    Update DetailsViewModel.kt.kt

--- RijsMuseum/app/src/main/java/com/example/rijsmuseum/viewmodel/DetailsViewModel.kt
+++ RijsMuseum/app/src/main/java/com/example/rijsmuseum/viewmodel/DetailsViewModel.kt
@@ -4,7 +4,10 @@ import androidx.lifecycle.ViewModel
 import com.example.network.getters.CollectionsDetailsGetter
 
 class DetailsViewModel: ViewModel() {
-    fun getCollectionsDetailsRequest(data: CollectionsDetailsGetter.DataReadyCallback) {
-        CollectionsDetailsGetter().getCollectionsDetailsRequest(data)
+    fun getCollectionsDetailsRequest(
+        data: CollectionsDetailsGetter.DataReadyCallback,
+        objectNumber: String
+    ) {
+        CollectionsDetailsGetter().getCollectionsDetailsRequest(data, objectNumber)
     }
 }
