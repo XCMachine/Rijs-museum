9a9c145d430f6086629071310be29ac95e7c4a0e
XCMachine <69400350+XCMachine@users.noreply.github.com>
2022-03-22 13:37:35 +0200

    Update DetailsFragment.kt.kt

--- RijsMuseum/app/src/main/java/com/example/rijsmuseum/viewmodel/DetailsViewModel.kt
+++ RijsMuseum/app/src/main/java/com/example/rijsmuseum/viewmodel/DetailsViewModel.kt
@@ -1,44 +1,23 @@
 package com.example.rijsmuseum.viewmodel
 
-import android.os.Bundle
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.network.getters.CollectionsDetailsGetter
-import com.example.network.models.Collections
-import com.example.rijsmuseum.adapter.RecyclerAdapter
-import com.example.rijsmuseum.fragments.DetailsFragment
+import com.example.network.models.CollectionsDetails
 
-class DetailsViewModel: ViewModel(), RecyclerAdapter.OnItemClickListener {
-    private var bundle = Bundle()
-    private var detailsFrag = DetailsFragment()
+class DetailsViewModel: ViewModel() {
+    var _dataTitle = MutableLiveData<String>()
+    val _cList = MutableLiveData<CollectionsDetails.ArtObject>()
 
     fun getCollectionsDetailsRequest(
-        data: CollectionsDetailsGetter.DataReadyCallback,
         objectNumber: String
     ) {
-        CollectionsDetailsGetter().getCollectionsDetailsRequest(data, objectNumber)
-    }
-
-    override fun onItemClick(cList: Collections.ArtObject) {
-        setBundleString(bundle, detailsFrag, cList)
-    }
-
-    fun setBundleString(
-        bundle: Bundle,
-        detailsFragment: DetailsFragment,
-        cList: Collections.ArtObject
-    ) {
-        this.bundle = bundle
-        bundle.putString(TEXT_KEY_OBJECT_NUMBER, cList.objectNumber)
-
-        setFragmentArgument(detailsFragment, bundle)
-    }
-
-    private fun setFragmentArgument(detailsFragment: DetailsFragment, bundle: Bundle) {
-        this.detailsFrag = detailsFragment
-        detailsFragment.arguments = bundle
-    }
-
-    companion object {
-        private const val TEXT_KEY_OBJECT_NUMBER = "objectNumber"
+        CollectionsDetailsGetter().getCollectionsDetailsRequest(
+            object : CollectionsDetailsGetter.DataReadyCallback {
+                override fun onDataReady(data: CollectionsDetails.ArtObject) {
+                    _cList.value = data
+                    _dataTitle.value = _cList.value?.title
+                }
+            }, objectNumber)
     }
 }
