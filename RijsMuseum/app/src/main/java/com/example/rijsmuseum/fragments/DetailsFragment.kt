3b852c8074a645df3e457bf81796c37af75a93c4
XCMachine <69400350+XCMachine@users.noreply.github.com>
2022-03-22 16:33:08 +0200

    Update DetailsFragment.kt

--- RijsMuseum/app/src/main/java/com/example/rijsmuseum/fragments/DetailsFragment.kt
+++ RijsMuseum/app/src/main/java/com/example/rijsmuseum/fragments/DetailsFragment.kt
@@ -5,16 +5,21 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.example.network.models.CollectionsDetails
+import com.example.rijsmuseum.MainActivity.Companion.OBJECT_NUMBER
 import com.example.rijsmuseum.R
+import com.example.rijsmuseum.databinding.FragmentCollectionDetailsBinding
 import com.example.rijsmuseum.viewmodel.DetailsViewModel
 
 
 class DetailsFragment : Fragment(R.layout.fragment_collection_details) {
-    private lateinit var titleText: TextView
     private lateinit var detailsViewModel: DetailsViewModel
+
+    private var _binding: FragmentCollectionDetailsBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,23 +30,28 @@ class DetailsFragment : Fragment(R.layout.fragment_collection_details) {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.fragment_collection_details, container, false)
+    ): View? {
+        _binding = FragmentCollectionDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        titleText = view.findViewById(R.id.fragmentTitle)
-        requireArguments().getString(TEXT_KEY_OBJECT_NUMBER)?.let { argumentString ->
+        requireArguments().getString(OBJECT_NUMBER)?.let { argumentString ->
             Log.d("Bundle", "Object number is: $argumentString")
-            detailsViewModel._dataTitle.observe(viewLifecycleOwner) { newString ->
-                titleText.text = newString
+
+            val stringObserver = Observer<CollectionsDetails.ArtObject> { newString ->
+                binding.fragmentTitle.text = newString.title
             }
+            detailsViewModel.cList.observe(viewLifecycleOwner, stringObserver)
 
             detailsViewModel.getCollectionsDetailsRequest(argumentString)
         } ?: Log.e("Error", "Object number is null")
     }
 
-    companion object {
-        private const val TEXT_KEY_OBJECT_NUMBER = "objectNumber"
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
