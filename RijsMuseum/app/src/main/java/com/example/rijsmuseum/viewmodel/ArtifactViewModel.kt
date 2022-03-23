e08ba3910ebe637011d61bf32b75a03099650de8
XCMachine <69400350+XCMachine@users.noreply.github.com>
2022-03-23 14:34:25 +0200

    Updated ArtifactViewModel.kt

--- RijsMuseum/app/src/main/java/com/example/rijsmuseum/viewmodel/ArtifactViewModel.kt
+++ RijsMuseum/app/src/main/java/com/example/rijsmuseum/viewmodel/ArtifactViewModel.kt
@@ -1,10 +1,20 @@
 package com.example.rijsmuseum.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.network.getters.CollectionsGetter
+import com.example.network.models.Collections
 
 class ArtifactViewModel: ViewModel() {
-    fun getCollectionsRequest(callback: CollectionsGetter.DataReadyCallback) {
-        CollectionsGetter().getCollectionRequest(callback)
+    private val _cList = MutableLiveData<List<Collections.ArtObject>>()
+    val cList: LiveData<List<Collections.ArtObject>> = _cList
+
+    fun getCollectionsRequest() {
+        CollectionsGetter().getCollectionRequest(object : CollectionsGetter.DataReadyCallback {
+            override fun onDataReady(data: List<Collections.ArtObject>) {
+                _cList.value = data
+            }
+        })
     }
 }
