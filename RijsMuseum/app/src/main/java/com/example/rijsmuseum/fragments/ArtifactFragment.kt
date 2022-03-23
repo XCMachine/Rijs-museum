987b73104e326017373dcbd22443fccad0fa07d2
XCMachine <69400350+XCMachine@users.noreply.github.com>
2022-03-23 11:18:50 +0200

    Update ArtifactFragment.kt and fragment_artifact.xml

--- RijsMuseum/app/src/main/java/com/example/rijsmuseum/fragments/ArtifactFragment.kt
+++ RijsMuseum/app/src/main/java/com/example/rijsmuseum/fragments/ArtifactFragment.kt
@@ -5,23 +5,67 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.network.getters.CollectionsGetter
+import com.example.network.models.Collections
 import com.example.rijsmuseum.R
+import com.example.rijsmuseum.adapter.RecyclerAdapter
+import com.example.rijsmuseum.databinding.FragmentArtifactBinding
+import com.example.rijsmuseum.viewmodel.ArtifactViewModel
 
-class ArtifactFragment : Fragment() {
+class ArtifactFragment : Fragment(), RecyclerAdapter.OnItemClickListener {
+    private lateinit var artifactViewModel: ArtifactViewModel
+    private lateinit var _binding: FragmentArtifactBinding
+
+    private lateinit var adapter: RecyclerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        artifactViewModel = ViewModelProvider(this).get(ArtifactViewModel::class.java)
+
+        artifactViewModel.getCollectionsRequest(object : CollectionsGetter.DataReadyCallback {
+            override fun onDataReady(data: List<Collections.ArtObject>) {
+                adapter.updateCollectionsData(data)
+            }
+        })
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_artifact, container, false)
+        _binding = FragmentArtifactBinding.inflate(inflater, container, false)
+        return _binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        adapterSettings()
+    }
+
+    private fun adapterSettings() {
+        adapter = RecyclerAdapter()
+        _binding.recyclerView.layoutManager = LinearLayoutManager(context)
+        _binding.recyclerView.adapter = adapter
+        adapter.setOnItemClickListener(this)
+    }
+
+    override fun onItemClick(cList: Collections.ArtObject) {
+        requireActivity().supportFragmentManager.beginTransaction().apply {
+            val detailsFragment = DetailsFragment().also { detailsFragment ->
+                val bundle = Bundle()
+                bundle.putString(OBJECT_NUMBER, cList.objectNumber)
+                detailsFragment.arguments = bundle
+            }
+            replace(R.id.fragmentContainerView, detailsFragment)
+            addToBackStack(ArtifactFragment::class.java.name)
+            commit()
+        }
+    }
+
+    companion object {
+        const val OBJECT_NUMBER = "objectNumber"
     }
 }
