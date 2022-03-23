c9de08e1a39656967939ad7dcb34df31640b1892
XCMachine <69400350+XCMachine@users.noreply.github.com>
2022-03-23 14:02:52 +0200

    Updated MainActivity.kt

--- RijsMuseum/app/src/main/java/com/example/rijsmuseum/MainActivity.kt
+++ RijsMuseum/app/src/main/java/com/example/rijsmuseum/MainActivity.kt
@@ -3,21 +3,16 @@ package com.example.rijsmuseum
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
 import com.example.rijsmuseum.databinding.ActivityMainBinding
 import com.example.rijsmuseum.fragments.ArtifactFragment
-import com.example.rijsmuseum.viewmodel.ArtifactViewModel
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var viewModel: ArtifactViewModel
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        viewModel = ViewModelProvider(this).get(ArtifactViewModel::class.java)
 
         //Button to get the list of artifacts
         binding.getButton.setOnClickListener {
